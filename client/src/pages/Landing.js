db71bead9ed344acd4c3a63cfe1d539c88791999
CSimbulan <c.simbulan@hotmail.com>
2021-07-27 16:31:20 -0400

    added landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,71 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-import { Typography } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
+import { Typography, makeStyles, Divider, Button } from "@material-ui/core";
 import { Route, Link } from "react-router-dom";
 
-import Ping from "./Ping";
-
-const landinPageStyle = theme => ({
-  landingContainer: {
-    margin: theme.spacing.unit * 2
+const useStyles = makeStyles((theme) => ({
+  outer: {
+    height: '100vh',
+    display: 'flex',
+    backgroundImage: `url(${"https://res.cloudinary.com/dqhd5slcy/image/upload/v1627415371/background_dijj6c.png"})`,
+    backgroundSize: 'cover'
+  },
+  container: {
+    position: 'relative',
+    display: 'flex',
+    width: '30%',
+    margin: 'auto auto auto auto',
+    boxShadow:
+      '0 0 0 1px rgba(0, 0, 0, 0.1), 0 2px 4px 1px rgba(0, 0, 0, 0.18)',
+    border: '2px solid #9e9e9e',
+    borderRadius: '10px',
+    textAlign: 'center',
+    backgroundColor: '#212121',
+    minHeight: '80vh',
+    [theme.breakpoints.down('md')]: {
+      width: '50%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+    },
+  },
+  white: {
+    color: 'white'
+  },
+  divider: {
+    width: '50%',
+    borderTop: '1px solid white',
+    marginTop: '30px',
+    marginBottom: '30px',
+    marginLeft: '25%'
+  },
+  landingButton: {
+    width: '100px',
+  },
+  inner: {
+    margin: 'auto'
+  },
+  m30: {
+    marginTop: '20px',
+    marginBottom: '20px'
   }
-});
+}))
 
-class LandingPage extends Component {
-  state = {
-    welcomeMessage: "Step 1: Run the server and refresh (not running)",
-    step: 0
-  };
+const LandingPage = () => {
+  const classes = useStyles();
+  const [page, setPage] = useState("default")
 
-  componentDidMount() {
-    fetch("/welcome")
-      .then(res => {
-        console.log(res);
-        if (res.status === 200) return res.json();
-        else throw Error("Couldn't connect to the server");
-      })
-      .then(res => {
-        this.setState({ welcomeMessage: res.welcomeMessage });
-        this.incrementStep();
-      })
-      .catch(err => {
-        console.log(err.message);
-      });
-  }
-
-  incrementStep = () => {
-    this.setState(prevState => ({ step: (prevState.step += 1) }));
-  };
-
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.landingContainer}>
-        <Typography>{this.state.welcomeMessage}</Typography>
-        {this.state.step >= 1 && (
-          <React.Fragment>
-            <Link to="/ping">Step 2: Click here </Link>
-            <Route
-              path="/ping"
-              render={props => {
-                return (
-                  <Ping
-                    {...props}
-                    incrementStep={this.incrementStep}
-                    step={this.state.step}
-                  />
-                );
-              }}
-            />
-          </React.Fragment>
-        )}
-        {this.state.step >= 3 && (
-          <Typography>All done! Now go make a pull request!</Typography>
-        )}
+  return (
+    <div className={classes.outer}>
+      <div className={classes.container}>
+        <div className={classes.inner}>
+          <Typography variant="h2" className={classes.white}>MintJack</Typography>
+          <Typography variant="body2" className={classes.white}>An online multiplayer web app</Typography>
+          <Divider className={classes.divider} />
+          <Typography variant="h4" className={classes.white + " " + classes.m30}>Have an account?</Typography>
+          <Button variant="contained" className={classes.landingButton}>Log In</Button>
+          <Typography variant="h4" className={classes.white + " " + classes.m30}>Need an account?</Typography>
+          <Button variant="contained" className={classes.landingButton}>Register</Button>
+          <Divider className={classes.divider} />
+          <Typography variant="body2" className={classes.white}>Don't want to register? Continute as guest.</Typography>
+        </div>
       </div>
-    );
-  }
+    </div>
+  )
+
 }
 
-export default withStyles(landinPageStyle)(LandingPage);
+export default LandingPage;
