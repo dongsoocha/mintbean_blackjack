8480873935d43e518483235011a91b2346d3a201
Chubbibanana <dongsoo.tchah@gmail.com>
2021-07-28 15:30:09 -0700

    score calculation logic done

--- server/gameLogic/game.js
+++ server/gameLogic/game.js
@@ -11,7 +11,7 @@ class Game {
         this.currentPlayer = 0;
         this.deck = new Deck();
         this.startGame = this.startGame.bind(this);
-        
+        this.takeTurn = this.takeTurn.bind(this);
     }
 
     startGame() {
@@ -52,10 +52,63 @@ class Game {
         }
     }
 
-    // calcScore(player);
+    calcScore(player) {
+        let score = 0;
+        let ace = false;
+        this.playerCards[player].forEach(card => {
+            let value = card.val;
+            switch (value){
+                case '1':
+                    ace = true;
+                    score += 11;
+                    break;
+                case '2':
+                    score += 2;
+                    break;
+                case '3':
+                    score += 3;
+                    break;
+                case '4':
+                    score += 4;
+                    break;
+                case '5':
+                    score += 5;
+                    break;
+                case '6':
+                    score += 6;
+                    break;
+                case '7':
+                    score += 7;
+                    break;
+                case '8':
+                    score += 8;
+                    break;
+                case '9':
+                    score += 9;
+                    break;
+                case '10':
+                    score += 10;
+                    break;
+                case 'J':
+                    score += 10;
+                    break;
+                case 'Q':
+                    score += 10;
+                    break;
+                case 'K':
+                    score += 10;
+                    break;
+            }
+        })
+        if (score > 21 && ace) {
+            score -= 10;
+        }
+        
+        console.log(this.playerCards[player], score);
+    };
 }
 
 let game = new Game(3);
 game.startGame();
-game.takeTurn();
+game.calcScore(1);
 
