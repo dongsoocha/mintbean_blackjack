19b7e818a8aef4d9346458744d9d78344320fab9
CSimbulan <c.simbulan@hotmail.com>
2021-07-29 21:04:29 -0400

    added mui cssbaseline

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { theme } from "./themes/theme";
 import LandingPage from "./pages/Landing";
 import Test from "./pages/Test";
+import Home from "./pages/Home";
 
 import { UserProvider } from "./contextProvider/user";
 
 import { SocketContext, socket } from './contextProvider/socket';
-
-import "./App.css";
 
 // TODO: Extract the background css out of individual pages to the app-wide styling
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <UserProvider>
-        <SocketContext.Provider value={socket}>
-          <BrowserRouter>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/test" component={Test} />
-          </BrowserRouter>
-        </SocketContext.Provider>
-      </UserProvider>
+      <CssBaseline>
+        <UserProvider>
+          <SocketContext.Provider value={socket}>
+            <BrowserRouter>
+              <Route path="/" exact component={LandingPage} />
+              <Route path="/test" component={Test} />
+              <Route path="/home" exact component={Home} />
+            </BrowserRouter>
+          </SocketContext.Provider>
+        </UserProvider>
+      </CssBaseline>
     </MuiThemeProvider >
   );
 }
