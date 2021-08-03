7bed5a212066e0bb97845c9783c2b287e18a00f4
Chubbibanana <dongsoo.tchah@gmail.com>
2021-08-02 22:30:01 -0700

    refactpr

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 //var http = require("http");
 //var server = http.createServer(app);
-socketConn.socketConnect(3001);
+
 
 app.use(passport.initialize());
 require("./server/config/passport")(passport);
@@ -37,4 +37,45 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${[port]}`));
+const server = app.listen(port, () => console.log(`Server is running on port ${[port]}`));
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const db = require("./server/config/keys.js").mongoURI;
+const users = require("./server/routes/api/users");
+const passport = require("passport");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const socketConn = require("./server/socket");
+const path = require("path");
+
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static("client/build"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+// }
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log("error"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//var http = require("http");
+//var server = http.createServer(app);
+
+app.use(passport.initialize());
+require("./server/config/passport")(passport);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => console.log(`Server is running on port ${[port]}`));
+socketConn.socketConnect(server);
+
