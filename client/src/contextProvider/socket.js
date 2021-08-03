05dcd1c7b3a8171fd2d5013e9385283af7e02de7
Chubbibanana <dongsoo.tchah@gmail.com>
2021-08-02 21:13:10 -0700

    edit socket

--- client/src/contextProvider/socket.js
+++ client/src/contextProvider/socket.js
@@ -1,5 +1,5 @@
 import socketio from 'socket.io-client';
 import { createContext } from 'react';
 
-export const socket = socketio.connect("ws://localhost:3001");
+export const socket = socketio.connect("http://mintjack.herokuapp.com/");
 export const SocketContext = createContext();
