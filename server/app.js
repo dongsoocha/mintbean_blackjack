69e3847d23065be2c66d2727e7ba63da026b3d24
Chubbibanana <dongsoo.tchah@gmail.com>
2021-08-02 19:27:37 -0700

    fix server host file

--- server/app.js
+++ server/app.js
@@ -36,6 +36,6 @@ require('./config/passport')(passport);
 app.use("/api/users", users);
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${[port]}`));
